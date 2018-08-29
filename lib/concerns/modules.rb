8bfd4cf57857ef01b95389cbc00ea57a4e6d0488
alexis101 <kondilisalexis@gmail.com>
2018-08-29 17:35:23 +0000

    automatically backed up by learn

--- lib/concerns/modules.rb
+++ lib/concerns/modules.rb
@@ -2,7 +2,7 @@ module Concerns
   module Findable
 
     def find_by_name(name)
-      self.all.detect {|s| s.name == name}
+      self.all.detect {|song| song.name == name}
     end
 
     def find_or_create_by_name(name)
